3b1a02e82b1e0fbd7e4ace2e76c0664bc687609f
zouyaoji <370681295@qq.com>
2022-09-03 09:54:16 +0800

    沙盒的例子czml可以传数组

--- src/components/datasource/czml/CzmlDataSource.vue
+++ src/components/datasource/czml/CzmlDataSource.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: zouyaoji@https://github.com/zouyaoji
+ * @Date: 2021-12-23 14:42:45
+ * @LastEditTime: 2022-09-03 09:51:26
+ * @LastEditors: zouyaoji
+ * @Description:
+ * @FilePath: \vue-cesium-v2\src\components\datasource\czml\CzmlDataSource.vue
+-->
 
 <script>
 import mixinDatasource from '../../../mixins/datasource/mixinDatasource'
@@ -6,7 +14,7 @@ export default {
   mixins: [mixinDatasource],
   props: {
     czml: {
-      type: [String, Object],
+      type: [String, Object, Array],
       required: true
     },
     options: Object
